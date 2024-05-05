1fe00defa9a45a766c8c247a9131137b14e8c6ba
ygryk603729 <163727616+ygryk603729@users.noreply.github.com>
2024-05-05 18:40:26 +0300

    Add files via upload

---
+++ lab2/helloTest_lab2.cpp
@@ -0,0 +1,58 @@
+﻿// helloTest_lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "trees.h"
+
+int main()
+{
+    Node* treap1 = NULL;
+    Node* treap2 = NULL;
+    Node* treap3 = NULL;
+
+    Node* treap4 = NULL;
+    Node* treap5 = NULL;
+
+    insert(&treap1, 5, 10);
+    insert(&treap1, 2, 8);
+    insert(&treap1, 8, 7);
+    insert(&treap1, 1, 6);
+
+    insert(&treap2, 7, 9);
+    insert(&treap2, 2, 5);
+    insert(&treap2, 8, 8);
+
+    insert(&treap3, 7, 20);
+    insert(&treap3, 2, 9);
+    insert(&treap3, 8, 12);
+    insert(&treap3, 6, 11);
+
+
+    printf("1 tree:");
+    printTree(treap1, 0);
+    printf("\n2 tree:");
+    printTree(treap2, 0);
+    printf("\n3 tree:");
+    printTree(treap3, 0);
+
+    splitTrees(&treap3, &treap4, &treap5, 6);
+    printf("\nSplited tree:");
+    printTree(treap5, 0);
+
+    mergeTrees(&treap1, &treap2);
+    printf("\nMerged tree:");
+    printTree(treap1, 0);
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ lab2/trees.cpp
@@ -0,0 +1,188 @@
+#include "trees.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+Node* сreateNode(int key, int priority) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->key = key;
+    newNode->priority = priority;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->parent = NULL;
+    return newNode;
+}
+
+void printTree(Node* root, int space) {
+    if (root == NULL) {
+        return;
+    }
+
+    int COUNT = 5;
+
+    space += COUNT;
+
+    printTree(root->right, space);
+    printf("\n");
+
+    for (int i = COUNT; i < space; i++) {
+        printf(" ");
+    }
+
+    printf("%d, %d\n", root->key, root->priority);
+
+    printTree(root->left, space);
+}
+
+void rotateRight(Node* node) {
+    Node* parent = node->parent;
+    Node* grandparent = parent->parent;
+    if (grandparent) {
+        if (grandparent->left == parent) {
+            grandparent->left = node;
+        }
+        else {
+            grandparent->right = node;
+        }
+    }
+    parent->left = node->right;
+    if (node->right) {
+        node->right->parent = parent;
+    }
+    node->right = parent;
+    parent->parent = node;
+    node->parent = grandparent;
+}
+
+void rotateLeft(Node* node) {
+    Node* parent = node->parent;
+    Node* grandparent = parent->parent;
+    if (grandparent) {
+        if (grandparent->left == parent) {
+            grandparent->left = node;
+        }
+        else {
+            grandparent->right = node;
+        }
+    }
+    parent->right = node->left;
+    if (node->left) {
+        node->left->parent = parent;
+    }
+    node->left = parent;
+    parent->parent = node;
+    node->parent = grandparent;
+}
+
+void insert(Node** root, int key, int priority) {
+    Node* newNode = сreateNode(key, priority);
+    if (*root == NULL) {
+        *root = newNode;
+        return;
+    }
+
+    Node* parent = NULL;
+    Node* current = *root;
+    while (current) {
+        parent = current;
+        if (key < current->key) {
+            current = current->left;
+        }
+        else {
+            current = current->right;
+        }
+    }
+
+    if (key < parent->key) {
+        parent->left = newNode;
+    }
+    else {
+        parent->right = newNode;
+    }
+    newNode->parent = parent;
+
+    while (newNode->parent && newNode->priority > newNode->parent->priority) {
+        if (newNode->parent->left == newNode) {
+            rotateRight(newNode);
+        }
+        else {
+            rotateLeft(newNode);
+        }
+    }
+}
+
+void mergeTrees(Node** t1, Node** t2) {
+    // Слияние двух treaps
+    Node* newRoot = NULL;
+    while (*t1 && *t2) {
+        if ((*t1)->priority > (*t2)->priority) {
+            insert(&newRoot, (*t1)->key, (*t1)->priority);
+            *t1 = (*t1)->right;
+        }
+        else {
+            insert(&newRoot, (*t2)->key, (*t2)->priority);
+            *t2 = (*t2)->right;
+        }
+    }
+
+    while (*t1) {
+        insert(&newRoot, (*t1)->key, (*t1)->priority);
+        *t1 = (*t1)->right;
+    }
+
+    while (*t2) {
+        insert(&newRoot, (*t2)->key, (*t2)->priority);
+        *t2 = (*t2)->right;
+    }
+
+    *t1 = newRoot;
+}
+
+void splitTrees(Node** root, Node** root1, Node** root2, int key) {
+    //insert(root, key, INT_MAX);
+    //printTree(*root, 0);
+    if ((*root)->key == key) {
+        (*root1) = (*root)->left;
+        (*root2) = (*root)->right;
+        if ((*root1) != NULL) {
+            (*root1)->parent = NULL;
+        }
+        if ((*root2) != NULL) {
+            (*root2)->parent = NULL;
+        }
+    }
+    else if ((*root)->key < key) {
+        (*root1) = (*root);
+        (*root2) = (*root)->right;
+        (*root1)->right = NULL;
+        if ((*root2) != NULL) {
+            (*root2)->parent = NULL;
+        }
+    }
+    else {
+        (*root1) = (*root)->left;
+        (*root2) = (*root);
+        (*root2)->left = NULL;
+        if ((*root1) != NULL) {
+            (*root1)->parent = NULL;
+        }
+    }
+}
+
+void AnswerTree(Node* root, int* result) {
+    if (root == NULL) {
+        return;
+    }
+
+    // Добавляем данные текущего узла к результату
+    *result = *result * 10 + root->key;
+
+    if (root->left != NULL) {
+        AnswerTree(root->left, result);
+    }
+
+    if (root->right != NULL) {
+        AnswerTree(root->right, result);
+    }
+}
